a9e5e17d0219ca05ffd3ab5486a60ddaa1aebb33
dam <dam@203P02>
2022-01-19 09:51:36 +0100

    Se ha creado rama nueva

---
+++ GIT/src/git/GIT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git;
+
+/**
+ *
+ * @author dam
+ */
+public class GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hola");
+        System.out.println("Hoy es dia 19 de enero");
+    }
+    
+}
